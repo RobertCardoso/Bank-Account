73814cdd5d65b426e7d2d3bee980e2ef56972107
RobertCardoso <robert.rcardoso@hotmail.com>
2022-04-25 20:25:17 -0500

    first

---
+++ bank.js
@@ -0,0 +1,19 @@
+class BankAccount {
+    accountNumber;
+    owner;
+    transactions;
+
+    constructor(accountNumber, owner){
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.transactions = []
+        
+
+
+    }
+
+    balance(){
+        
+    }
+}
+
