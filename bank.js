65efd39ba8d06047bf969215aa8afd89900434a3
RobertCardoso <robert.rcardoso@hotmail.com>
2022-04-28 21:40:55 -0500

    done

--- bank.js
+++ bank.js
@@ -3,17 +3,53 @@ class BankAccount {
     owner;
     transactions;
 
-    constructor(accountNumber, owner){
+    constructor(accountNumber, owner) {
         this.accountNumber = accountNumber;
         this.owner = owner;
         this.transactions = []
-        
+
 
 
     }
 
-    balance(){
-        
+    balance() {
+        let bal = 0;
+        for (let i = 0; this.transactions.length; i++) {
+            bal += this.transactions[i].amount;
+        }
+    };
+
+    chargeMoney(payee, amount) {
+        let Balance = this.balance();
+
+        if (amount > Balance) {
+            return
+        } else {
+            let charge = new Transactions(-1 * amount, payee);
+            this.transactions.push(charge);
+        }
+
+
+    }
+
+    depositMoney(amount) {
+        if (amout > 0) {
+            let deposit = new Transactions(amount, 'Deposit');
+            this.transactions.push(deposit);
+        }
+    }
+}
+
+
+class Transactions {
+    amount;
+    payee;
+    date;
+
+    constructor(amount, payee) {
+        this.amount = amount
+        this.payee = payee
+        this.date = new Date();
     }
 }
 
